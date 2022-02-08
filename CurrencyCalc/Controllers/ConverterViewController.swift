3b34b4e99526772fa849341f22decfc796559188
bobermiha <bobermiha@yandex.ru>
2022-02-08 20:50:02 +0500

    setUpTextFields

--- CurrencyCalc/Controllers/ConverterViewController.swift
+++ CurrencyCalc/Controllers/ConverterViewController.swift
@@ -14,16 +14,29 @@ class ConverterViewController: UIViewController {
     
     private var firstValuteTF: UITextField = {
         let textField = UITextField()
+        textField.borderStyle = .none
+        textField.layer.cornerRadius = 7.5
+        textField.layer.borderWidth = 2.0
+        textField.clipsToBounds = true
+        textField.layer.borderColor = UIColor(red: 184/255, green: 92/255, blue: 56/255, alpha: 1.0).cgColor
+        textField.textAlignment = .center
+        textField.placeholder = "Введите значение "
+        textField.font = UIFont(name: "Rockwell", size: 15)
         return textField
     }()
     
-    private var secondFaluteTF: UITextField = {
+    private var secondValuteTF: UITextField = {
         let textField = UITextField()
+        textField.textAlignment = .right
+        textField.borderStyle = .line
+        textField.placeholder = "Введите значение"
+        textField.font = UIFont(name: "Rockwell", size: 15)
         return textField
     }()
     
     private var backgroudImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.image = UIImage(named: "exchange")
         return imageView
     }()
     
@@ -62,12 +75,43 @@ class ConverterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setUpBackgroundImageView()
+        setUpFirstTextField()
+        setUpSecondTextField()
     }
     
     
     // MARK: UI Configuration methods
     
+    private func setUpBackgroundImageView() {
+        view.addSubview(backgroudImageView)
+        backgroudImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(150)
+            make.width.equalTo(150)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(35)
+        }
+    }
     
+    private func setUpFirstTextField() {
+        view.addSubview(firstValuteTF)
+        firstValuteTF.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(25)
+            make.top.equalTo(backgroudImageView.snp.bottom).inset(-80)
+            make.width.equalTo(180)
+            make.height.equalTo(40)
+            
+        }
+    }
     
-
+    private func setUpSecondTextField() {
+        view.addSubview(secondValuteTF)
+        secondValuteTF.snp.makeConstraints { make in
+            make.right.equalToSuperview().inset(25)
+            make.top.equalTo(backgroudImageView.snp.bottom).inset(-80)
+            make.width.equalTo(150)
+            make.height.equalTo(40)
+        }
+    }
+    
 }
